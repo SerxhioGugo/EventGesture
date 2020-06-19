cebe3d15431a0777c2b776f320d8c2960c13068a
SerxhioGugo <serxhio.gugo@gmail.com>
2020-06-19 13:02:39 -0400

    add cloud messaging

--- EventGesture/ViewController.swift
+++ EventGesture/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    lazy var phoneStateLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Phone State \nSUSPENDED"
+        label.font = UIFont(name: "Avenir-Black", size: 45)
+        label.numberOfLines = 2
+        label.textColor = .white
+        label.textAlignment = .center
+        return label
+    }()
+    
+    //rgba(39, 174, 96,1.0)
+    //rgba(243, 156, 18,1.0)
+    //rgba(231, 76, 60,1.0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        view.backgroundColor = UIColor(red: 39/255, green: 174/255, blue: 96/255, alpha: 1.0)
+          view.backgroundColor = UIColor(red: 231/255, green: 76/255, blue: 60/255, alpha: 1.0)
+        view.addSubview(phoneStateLabel)
+        phoneStateLabel.snp.makeConstraints { (make) in
+            make.center.equalToSuperview()
+        }
     }
 
 
