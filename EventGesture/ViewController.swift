6f98fbc125b17b0750305f9ae78672a5f62d4058
SerxhioGugo <serxhio.gugo@gmail.com>
2020-06-19 10:54:16 -0400

    Initial Commit

---
+++ EventGesture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EventGesture
+//
+//  Created by Serxhio Gugo on 6/19/20.
+//  Copyright © 2020 Serxhio Gugo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
